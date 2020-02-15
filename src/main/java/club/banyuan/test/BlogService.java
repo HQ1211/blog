fdd55d52d3df9e556102ad2ae850e1601436edbb
HQ <hq@hqdeMacBook-Air.local>
2020-02-15 18:26:42 +0800

    del

--- src/main/java/club/banyuan/test/BlogService.java
+++
@@ -1,9 +0,0 @@
-//package club.banyuan.test;
-//
-//public class BlogService {
-//    private User user;
-//
-//    String findBlogs(User user){
-//        return "user";
-//    }
-//}
--- src/main/java/club/banyuan/test/UserService.java
+++
@@ -1,4 +0,0 @@
-//package club.banyuan.test;
-//
-//public class UserService {
-//}
